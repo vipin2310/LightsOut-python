919f6f9be8cfe53f988e1131efa9444b8b107d3b
Vipin Singh <vipin2310@users.noreply.github.com>
2022-12-06 20:02:00 +0100

    basic structure for creating gui

---
+++ src/main/python/main.py
@@ -0,0 +1,11 @@
+from ui.view_manager import ViewManager
+from ui.views import MainMenuView
+
+if __name__ == "__main__":
+    manager = ViewManager.get_instance()
+    manager.set_view(MainMenuView())
+    manager.set_width(650)
+    manager.set_height(650)
+    manager.set_min_width(500)
+    manager.set_min_height(500)
+    manager.set_main_title("Lights out")
--- src/main/python/ui/view_manager.py
+++ src/main/python/ui/view_manager.py
@@ -1,13 +1,47 @@
 import PySimpleGUI
+from views import View
+from typing import Self
 
 class ViewManager:
     """Manages the views for the game. This class follows the Singleton pattern.
     """
+    
+    def get_instance() -> Self:
+        """Returns a single instance of this class.
+
+        Returns:
+            ViewManager: current ViewManager
+        """
+        return ViewManager()
     
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(ViewManager, cls).__new__(cls)
         return cls.instance
     
-    def __init__(self, view):
-        self.view
+    def __init__(self):
+        self.view = None
+        self.width = -1
+        self.height = -1
+        self.min_width = -1
+        self.min_height = -1
+        self.main_title = ""
+        pass
+    
+    def set_view(self, view : View) -> None:
+        self.view = view
+        
+    def set_width(self, width : int) -> None:
+        pass
+    
+    def set_height(self, height : int) -> None:
+        pass
+    
+    def set_min_width(self, min_width : int) -> None:
+        pass
+    
+    def set_min_height(self, min_height : int) -> None:
+        pass
+    
+    def set_main_title(self, title : str) -> None:
+        pass
