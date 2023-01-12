67bd75d94049cf05c8d34f3498ff961231e4c6e5
Vipin Singh <vipin2310@users.noreply.github.com>
2023-01-12 17:41:00 +0100

    Changed ViewManager to non-object (no class)

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -2,13 +2,12 @@ from ui.view_manager import ViewManager
 from ui.views.main_menu_view import MainMenuView
 
 if __name__ == "__main__":
-    manager = ViewManager.get_instance()
-    manager.set_view(MainMenuView())
-    manager.set_width(650)
-    manager.set_height(650)
-    manager.set_min_width(500)
-    manager.set_min_height(500)
-    manager.set_main_title("Lights out")
+    ViewManager.set_view(MainMenuView(), keep_attributes = False)
+    ViewManager.set_width(650)
+    ViewManager.set_height(650)
+    ViewManager.set_min_width(500)
+    ViewManager.set_min_height(500)
+    ViewManager.set_main_title("Lights out")
     
-    manager.start_event_handler()
-    manager.terminate()
+    ViewManager.start_event_handler()
+    ViewManager.terminate()
--- src/main/python/ui/view_manager.py
+++ src/main/python/ui/view_manager.py
@@ -1,108 +1,109 @@
 from ui.views.view import View
-from typing import Self
 
 class ViewManager:
-    """Manages the views for the game. This class follows the Singleton pattern.
+    """Manages the views for the game.
     """
     
-    def get_instance() -> Self:
-        """Returns a single instance of this class.
-
-        Returns:
-            ViewManager: current ViewManager
-        """
-        return ViewManager()
+    # Variables for the ViewManager
+    _view : View = None
+    _width : int = -1
+    _height : int = -1
+    _min_width : int = -1
+    _min_height : int = -1
+    _main_title : str = ""
+    _terminated : bool = False
     
-    def __new__(cls):
-        """Prevents the creation of new instances if there is already one.
-
-        Returns:
-            ViewManager instance.
-        """
-        
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(ViewManager, cls).__new__(cls)
-        return cls.instance
-    
-    def __init__(self):
-        """Constructor.
-        """
-        
-        self.view = None
-        self.width = -1
-        self.height = -1
-        self.min_width = -1
-        self.min_height = -1
-        self.main_title = ""
-        self.terminated = False
-    
-    def set_view(self, view : View) -> None:
-        """Sets a new view for the application. Closes the previous view.
+    def set_view(view : View, keep_attributes = True) -> None:
+        """Sets a new view for the application. Closes the previous view. If wanted, the last set attributes will be applied to the next view.
 
         Args:
             view (View): The view to be set.
         """
         
-        if self.view != None:
-            self.view.close_window()
+        if ViewManager._view != None:
+            ViewManager._view.close_window()
         
-        self.view = view
-        self.view.create_window()
+        ViewManager._view = view
+        ViewManager._view.create_window()
         
-    def set_width(self, width : int) -> None:
+        if keep_attributes:
+            if -1 in (ViewManager._width, ViewManager._height, ViewManager._min_width, ViewManager._min_height):
+                raise ValueError("One or more attributes have not been specified. Maybe you want to set keep_attributes to False.")
+            
+            ViewManager._view.set_width(ViewManager._width)
+            ViewManager._view.set_height(ViewManager._height)
+            ViewManager._view.set_min_width(ViewManager._min_width)
+            ViewManager._view.set_min_height(ViewManager._min_height)
+        
+    def set_width(width : int) -> None:
         """Sets the current width of the application window.
 
         Args:
             width (int): The width of the application window.
         """
+        if not ViewManager._view:
+            raise ValueError("The view must be initialized first.")
         
-        self.width = width
-        self.view.set_width(self.width)
+        ViewManager._width = width
+        ViewManager._view.set_width(width)
     
-    def set_height(self, height : int) -> None:
+    def set_height(height : int) -> None:
         """Sets the current height of the application window.
 
         Args:
             height (int): The height of the application window.
         """
+        if not ViewManager._view:
+            raise ValueError("The view must be initialized first.")
         
-        self.height = height
-        self.view.set_height(height)
+        ViewManager._height = height
+        ViewManager._view.set_height(height)
     
-    def set_min_width(self, min_width : int) -> None:
+    def set_min_width(min_width : int) -> None:
         """Sets the minimum width of the application window.
 
         Args:
             min_width (int): The minimum width of the application window.
         """
+        if not ViewManager._view:
+            raise ValueError("The view must be initialized first.")
         
-        self.min_width = min_width
-        self.view.set_min_width(self.min_width)
+        ViewManager._min_width = min_width
+        ViewManager._view.set_min_width(min_width)
     
-    def set_min_height(self, min_height : int) -> None:
+    def set_min_height(min_height : int) -> None:
         """Sets the minimum height of the application window.
 
         Args:
             min_height (int): The minimum height of the application window.
         """
+        if not ViewManager._view:
+            raise ValueError("The view must be initialized first.")
         
-        self.min_height = min_height
-        self.view.set_min_height(self.min_height)
+        ViewManager._min_height = min_height
+        ViewManager._view.set_min_height(min_height)
     
-    def set_main_title(self, main_title : str) -> None:
+    def set_main_title(main_title : str) -> None:
         """Sets the title for this application window.
 
         Args:
             main_title (str): The title for the application window.
         """
+        if not ViewManager._view:
+            raise ValueError("The view must be initialized first.")
         
-        self.main_title = main_title
-        self.view.set_main_title(self.main_title)
+        ViewManager._main_title = main_title
+        ViewManager._view.set_main_title(main_title)
         
-    def start_event_handler(self) -> None:
-        self.view.read_events()
+    def start_event_handler() -> None:
+        """Starts the event handling for the current view.
+        """
+        if not ViewManager._view:
+            raise ValueError("The view must be initialized first.")
         
-    def terminate(self) -> None:
-        if not self.terminated and self.view != None:
-            self.terminated = True
-            self.view.close_window()
+        ViewManager._view.read_events()
+        
+    def terminate() -> None:
+        if not ViewManager._terminated and ViewManager._view != None:
+            ViewManager._terminated = True
+            ViewManager._view.close_window()
